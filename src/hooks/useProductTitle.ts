ddce2d3218ae8b4b900cd3d9e09d6fc0b084e6ee
stiven trujillo <strujillodev@gmail.com>
2024-10-10 16:17:43 -0500

    feat: more keywords

--- src/hooks/useProductTitle.ts
+++ src/hooks/useProductTitle.ts
@@ -150,7 +150,7 @@ export const useProductTitle = () => {
               role: "user",
               content: `10 títulos, para crear los títulos, solo ve adicionando todas las palabras claves que te doy, generando varias combinaciones coherentes y concisas y comenzando siempre por el nombre del producto, palabras claves:[${
                 values ? ` ${values}, ` : ""
-              } ${valuesImage} ${valuesTrends}]. La anterior lista de palabras claves pueden contener palabras claves que no representan en absoluto al producto **${productName}**, por favor, identifica cuales si, y utilízalas para los 10 títulos generados. Procura que el titulo generado no tenga palabras repetidas, como por ejemplo si tenemos las palabras claves [peluche gato], [peluche gato lucifer], [peluche gato original] No generes un titulo asi: [peluche gato peluche gato lucifer peluche gato original] si no asi: [peluche gato lucifer original].`,
+              } ${valuesImage} ${valuesTrends}]. La anterior lista de palabras claves pueden contener palabras claves que no representan en absoluto al producto **${productName}**, por favor, identifica cuales si, y utilízalas para los 10 títulos generados. Procura que el titulo generado no tenga palabras repetidas, como por ejemplo si tenemos las palabras claves [peluche gato], [peluche gato lucifer], [peluche gato original] No generes un titulo asi: [peluche gato peluche gato lucifer peluche gato original] si no asi: [peluche gato lucifer original]. Utiliza la mayor cantidad de palabras claves para cada título.`,
             },
           ],
         };
