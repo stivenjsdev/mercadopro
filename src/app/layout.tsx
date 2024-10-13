e3af477719adb2f4e205bcb949e9f5106ec0712a
stiven trujillo <strujillodev@gmail.com>
2024-10-12 21:34:01 -0500

    feat: move react query in a provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
-"use client";
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import type { Metadata } from "next";
+import ReactQueryProvider from "@/utils/providers/ReactQueryProvider";
+import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { useState } from "react";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -18,51 +14,47 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-// export const metadata: Metadata = {
-//   title: "Mercadopro",
-//   description:
-//     "Tu Titulo y Descripción de Producto Generado con IA creado por Sergio Jiménez",
-//   keywords: [
-//     "mercadolibre",
-//     "mercadopro",
-//     "ia",
-//     "generador",
-//     "titulo",
-//     "descripción",
-//     "producto",
-//     "sergio jimenez",
-//   ],
-//   openGraph: {
-//     title: "MercadoPro",
-//     description:
-//       "Tu Titulo y Descripción de Producto Generado con IA creado por Sergio Jiménez.",
-//     url: "https://mercadopro.netlify.app/",
-//     images: [
-//       {
-//         url: "/images/logomercadopro.png",
-//         width: 400,
-//         height: 157,
-//         alt: "Logo MercadoPro",
-//       },
-//     ],
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Mercadopro",
+  description:
+    "Tu Titulo y Descripción de Producto Generado con IA creado por Sergio Jiménez",
+  keywords: [
+    "mercadolibre",
+    "mercadopro",
+    "ia",
+    "generador",
+    "titulo",
+    "descripción",
+    "producto",
+    "sergio jimenez",
+  ],
+  openGraph: {
+    title: "MercadoPro",
+    description:
+      "Tu Titulo y Descripción de Producto Generado con IA creado por Sergio Jiménez.",
+    url: "https://mercadopro.netlify.app/",
+    images: [
+      {
+        url: "/images/logomercadopro.png",
+        width: 400,
+        height: 157,
+        alt: "Logo MercadoPro",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
